4bbbdf73fadf1cb9cd04a0593d56b682433f0888
daniel almeida (CTW) <daniel.almeida@ctw.bmwgroup.com>
2024-02-04 18:57:03 +0000

    Added submit button

--- main.js
+++ main.js
@@ -1,75 +1,80 @@
 let database;
+let addDoc;
 let guestNames;
 let currentSlide = 0;
 
 const GUESTS = {
-    'paula': "Mamã",
-    'luis': "Luís",
-    'nuno': "Papá",
-    'alice': "Alice",
-    'nuno-miguel': 'Miguel',
-    'rozzy': 'Rozzy',
-    'natercia': 'Natércia',
-    'joao-paulo': 'João',
-    'sofia': 'Sofia',
-    'jorge': 'Jorge',
-    'paula-monteiro': 'Mamã',
-    'gusto': 'Papá',
-    'vovo': 'Vovó',
-    'tiago': 'Tiago',
-    'carlos': 'Carlos',
-    'tuxa': 'Tuxa',
-    'bela': 'Bela',
-    'joao-eng': 'João',
-    'rui': 'Rui',
-    'nani': 'Nani',
-    'miguel': 'Miguel',
-    'joao': 'João',
-    'beatriz': 'Beatriz',
-    'carina': 'Carina',
-    'hugo': 'Hugo',
-    'paulo': 'Paulo',
-    'raquel': 'Raquel',
-    'andre': 'André',
-    'joana': 'Joana',
-    'margarida': 'Margarida',
-    'mariana': 'Mariana',
-    'mimi': 'Mimi',
-    'mica': 'Micá',
-    'iolanda': 'Iolanda',
-    'ricardo': 'Ricardo',
-    'carlota': 'Carlota',
-    'andreia': 'Andreia',
-    'fernando': 'Fernando',
-    'lu': 'Lu',
-    'alex': 'Alex',
-    'catarina': 'Catarina'
+  'paula': "Mamã",
+  'luis': "Luís",
+  'nuno': "Papá",
+  'alice': "Alice",
+  'nuno-miguel': 'Miguel',
+  'rozzy': 'Rozzy',
+  'natercia': 'Natércia',
+  'joao-paulo': 'João',
+  'sofia': 'Sofia',
+  'jorge': 'Jorge',
+  'paula-monteiro': 'Mamã',
+  'gusto': 'Papá',
+  'vovo': 'Vovó',
+  'tiago': 'Tiago',
+  'carlos': 'Carlos',
+  'tuxa': 'Tuxa',
+  'bela': 'Bela',
+  'joao-eng': 'João',
+  'rui': 'Rui',
+  'nani': 'Nani',
+  'miguel': 'Miguel',
+  'joao': 'João',
+  'beatriz': 'Beatriz',
+  'carina': 'Carina',
+  'hugo': 'Hugo',
+  'paulo': 'Paulo',
+  'raquel': 'Raquel',
+  'andre': 'André',
+  'joana': 'Joana',
+  'margarida': 'Margarida',
+  'mariana': 'Mariana',
+  'mimi': 'Mimi',
+  'mica': 'Micá',
+  'iolanda': 'Iolanda',
+  'ricardo': 'Ricardo',
+  'carlota': 'Carlota',
+  'andreia': 'Andreia',
+  'fernando': 'Fernando',
+  'lu': 'Lu',
+  'alex': 'Alex',
+  'catarina': 'Catarina'
 }
 
 function greetGuests() {
-    const guests = new URLSearchParams(window.location.search)?.get('guests')?.split(',');
-    guestNames = [];
+  const guests = new URLSearchParams(window.location.search)?.get('guests')?.split(',');
+  guestNames = [];
 
-    if (!guests) return
+  if (!guests) return
 
-    for (let param of guests.values()) {
-        if (GUESTS[param]) {
-            guestNames.push(GUESTS[param]);
-        }
+  for (let param of guests.values()) {
+    if (GUESTS[param]) {
+      guestNames.push({
+        name: GUESTS[param],
+        id: param
+      });
     }
+  }
 
-    document.getElementById('greeting').textContent =
-        `Olá, ${guestNames.join(' e ')}\nFicaremos muito felizes por vos receber`;
+  document.getElementById('greeting').textContent =
+    `Olá, ${guestNames.map(v => v.name).join(', ')}\nFicaremos muito felizes por vos receber`;
 }
 
 document.addEventListener('FirestoreInitialized', (e) => {
-    database = e.detail.database;
+  database = e.detail.database;
+  addDoc = e.detail.addDoc
 });
 
 
 function scrollInto(section) {
-    const element = document.getElementById(section);
-    element.scrollIntoView({ behavior: 'smooth' });
+  const element = document.getElementById(section);
+  element.scrollIntoView({ behavior: 'smooth' });
 }
 
 const slides = document.querySelectorAll(".main__slider__slide");
@@ -94,5 +99,39 @@ function prevSlide() {
   showSlide(currentSlide);
 }
 
+function buildConfirmation() {
+  const confirmation = document.getElementById('confirmation-list');
+  guestNames.forEach(guest => {
+    confirmation.innerHTML = `${confirmation.innerHTML}
+      <div style="display: flex">
+      <input type="checkbox" name="${guest.id}" id="${guest.id}"/>
+      <label for="${guest.id}">${guest.name}</label>
+    </div>`
+  })
+}
+
+function handleSubmit() {
+  const allergies = document.getElementById('allergies').value;
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    const id = checkbox.id
+    const isChecked = checkbox.checked
+    const guest = guestNames.find(g => g.id === id);
+    guest.confirmed = isChecked;
+    guest.allergies = allergies;
+  })
+
+  guestNames.forEach(guest => {
+    addDoc(database, guest)
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+  })
+}
+
 showSlide(currentSlide);
 greetGuests()
+buildConfirmation();
